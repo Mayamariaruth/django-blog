ea98e23bf02371464fee75970296bc7c487c51fb
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2024-02-05 16:27:42 +0800

    prepare for heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u9czxm0!s=%wdit0ybjptu5n8(qk@b(8k2^h3u!%^82_@0$&1@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [.herokuapp.com]
 
 
 # Application definition
