d3ac8518e7bb54d5680f5c9f4b7f8893c233d109
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2024-02-06 19:06:43 +0800

    add comment model

--- codestar/settings.py
+++ codestar/settings.py
@@ -94,7 +94,7 @@ dj_database_url.parse(os.environ.get("DATABASE_URL"))
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:8000/*",
+    "http://127.0.0.1:8000/",
     "https://*.herokuapp.com"
 ]
 
